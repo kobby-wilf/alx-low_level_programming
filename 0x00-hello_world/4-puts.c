e31bc8b44d4e121eb6c76b8215eab468144dea88
kobby-wilf <coloradobank1234@gmail.com>
2022-09-09 02:47:31 -0700

    A c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - Entry point
@@ -10,9 +9,8 @@
  *
  * 
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
