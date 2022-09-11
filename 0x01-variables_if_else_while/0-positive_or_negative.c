fe74f3b94dea643a9da7ba9978c1073882c568ae
kobby-wilf <coloradobank1234@gmail.com>
2022-09-11 11:56:37 -0700

    First if else program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-	if(n == 0)
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
@@ -27,7 +27,7 @@
 	{
 		printf("%d is positive\n", n);
 	}
-	return(0);
+	return (0);
 }
 
 
