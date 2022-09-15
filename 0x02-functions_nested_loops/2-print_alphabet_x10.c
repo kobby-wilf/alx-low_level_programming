d37dc670e87daabc29ebe876facc79eae0676a90
kobby-wilf <coloradobank1234@gmail.com>
2022-09-15 04:48:39 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - functions to print abc 10 times
+ *
+ * Return: 0 
+ */
+void print_alphatbet_x10(voi)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n);
+	}
+}
