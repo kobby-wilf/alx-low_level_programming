78068584f58f9f70c8ed5eb3ff7540be91081429
kobby-wilf <coloradobank1234@gmail.com>
2022-09-20 16:21:12 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _stren -return the length of a string
+ * _strlen - returns the length of a string
  * @s: string
- * Return: returns length as integer ;
+ * Return: returns length as integer;
  */
 
 int _strlen(char *s)
