1c1977360527c7c488e679115f2ba788b8ae0bc7
kobby-wilf <coloradobank1234@gmail.com>
2022-09-26 07:47:54 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * _memset - fills a memory block with a consant byte
- * @s: address to memory block 
+ * @s: address to memory block
  * @b: char to be used
  * @n: number of bytes to be used
  *
@@ -11,10 +11,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		s[n - 1] = b;
-		n--;
+		s[i] = b;
+		i++;
 	}
+
 	return (s);
 }
