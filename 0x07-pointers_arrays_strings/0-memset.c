8baca8f4e660f6660a5075628b27f323a53146c4
kobby-wilf <coloradobank1234@gmail.com>
2022-09-26 06:06:15 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,12 +13,10 @@
 
 void *_memset(void *s, int c, size_t n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
-
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-
-	return (memory);
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,14 +4,14 @@
 /**
  * File: main.h
  * description: Header file containing prototypes for all functions
- 	used in the 0x06-pointers_arrays_strings directory.
+ 	used in the 0x07-pointers_arrays_strings directory.
 *
 */
 
 #include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -21,4 +21,4 @@ void print_chessboard(cha (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+#endif /*MAIN_H*/
