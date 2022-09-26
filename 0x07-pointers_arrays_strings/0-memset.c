834d3d3480a0368fc986f57473764ed8613b1b68
kobby-wilf <coloradobank1234@gmail.com>
2022-09-26 07:38:55 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
- * _memset - Fills a memory block with a consant byte
+ * _memset - fills a memory block with a consant byte
  * @s: address to memory block 
  * @b: char to be used
  * @n: number of bytes to be used
  *
- * Return: A pointer the memory block.
+ * Return: pointer to the memory block.
  */
 
 char *_memset(char *s, char b, unsigned int n)
